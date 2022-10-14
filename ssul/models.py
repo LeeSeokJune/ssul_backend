9c8a5e6b0c212e622df745322d488ea9d36d83de
LeeSeokJune <indians1720@gmail.com>
2022-10-14 20:39:48 +0900

    models

--- ssul/models.py
+++ ssul/models.py
@@ -1,7 +1,23 @@
+from email.policy import default
 from django.db import models
 from inheritance.models import TimeStampedModel
 # Create your models here.
 
 
+class Category(models.Model):
+    id = models.AutoField(primary_key=True)
+    category = models.CharField(max_length=20)
+
+
 class SsulModel(TimeStampedModel):
-    order = models.AutoField(primary_key=True)
+    id = models.AutoField(primary_key=True)
+    # user
+    # category
+    contents = models.CharField(max_length=1000)
+    left_option = models.CharField(max_legth=20)
+    left_count = models.IntegerField(default=0)
+    right_option = models.CharField(max_legth=20)
+    right_count = models.IntegerField(default=0)
+    like_count = models.IntegerField(default=0)
+    report_count = models.IntegerField(default=0)
+    view_count = models.IntegerField(default=0)

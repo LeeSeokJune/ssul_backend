9642a07867dd1a869a89e0ff7bae5350a5344d1f
LeeSeokJune <indians1720@gmail.com>
2022-10-25 17:26:13 +0900

    models

--- ssul/views.py
+++ ssul/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import SsulModel
+from user.models import CustomUser
 # Create your views here.
 
 
 @api_view(['POST'])
-def ssul(request):
-    return Response('asdf')
+def register_ssul(request):
+    if request.method == 'POST':
+        print('register_ssul')
+        return Response('asdf')
+
+
+@api_view(['POST'])
+def get_ssul(request):
+    if request.method == 'POST':
+        print('get_ssul')
+        return Response()
